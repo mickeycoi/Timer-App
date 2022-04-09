e93b1fadfe0e6ada4b5c7fa37cdf1f499b547d65
mickeycoi <datnguyenthanh1410@gmail.com>
2022-04-09 09:21:52 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  // useEffect(() => {
+  //   setInterval(setSeconds(seconds + 1), 1000);
+  // }, [seconds]);
+  const handelStop = () => {};
+  const handelStart = () => {};
+  const handelReset = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer-app">
+      <h1>CODER TIMER</h1>
+      <div className="timer">
+        <div className="timer-display">
+          <p>
+            {hours}:{minutes}:{seconds}
+          </p>
+        </div>
+        <div className="button-wrapper">
+          <button onClick={handelStop}>STOP</button>
+          <button onClick={handelStart}>START</button>
+          <button onClick={handelReset}>RESET</button>
+        </div>
+      </div>
     </div>
   );
 }
